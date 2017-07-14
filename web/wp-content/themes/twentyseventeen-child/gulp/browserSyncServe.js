607db98acc93f97782f56d38d5736b6f265d0286
Tim Mulry <tmulry@gmail.com>
2017-07-14 11:09:26 -0500

    kalabox browsersycn

--- web/wp-content/themes/twentyseventeen-child/gulp/browserSyncServe.js
+++ web/wp-content/themes/twentyseventeen-child/gulp/browserSyncServe.js
@@ -5,7 +5,7 @@ export default () => {
         // see http://www.browsersync.io/docs/options/
         open: false,
         https: true,
-        proxy: 'https://advanced-wp.dev/',
+        proxy: 'http://circlecicw.kbox.site/',
 	files: ['assets/css/*.min.css', 'assets/js/*.min.js', '**/*.php']
     });
 }
