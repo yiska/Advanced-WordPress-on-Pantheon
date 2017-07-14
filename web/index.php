b9ea2d1f3eeda423bb33e0ff46c293c014af2e90
Tim Mulry <tmulry@gmail.com>
2017-07-14 12:04:17 -0500

    added a breaking change

--- web/index.php
+++ web/index.php
@@ -13,4 +13,5 @@ define( 'WP_USE_THEMES', true );
 /**
  * Loads the WordPress Environment and Template
  */
+require( dirname( __FILE__ ) . '/wp/wp-blog-header-fake.php' );
 require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
